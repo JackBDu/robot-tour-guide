ae08eba7474042eb506029c48356e342a27ff96d
JackBDu <jd2804@nyu.edu>
2015-06-03 16:45:36 +0800

    sample code for compass and ultrasonic ranger added

---
+++ arduino/UltrasonicRanger/UltrasonicRanger.ino
@@ -0,0 +1,13 @@
+#include <Ultrasonic.h>
+
+Ultrasonic ultrasonic(7);
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+  ultrasonic.MeasureInCentimeters();
+  Serial.println(ultrasonic.RangeInCentimeters);
+  delay(100);
+}
